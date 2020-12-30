3a6605d65f284c9068e6a30f8502c16a2a95a3a7
liyp <794508986@qq.com>
2020-12-30 08:41:48 +0800

    管理员与学生登陆界面！

--- src/Manager/adminLogin.java
+++ src/Manager/adminLogin.java
@@ -2,41 +2,120 @@ package Manager;
 
 import db.*;
 
+import javax.swing.*;
+import java.awt.*;
 import java.sql.*;
-import java.sql.Statement;
-import java.util.Scanner;
 
-public class adminLogin {
-    private static DbCon dbCon = new DbCon();
 
-    public static void main(String[] args) throws Exception {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("请输入账号：");
-        String count = scanner.next();
-        System.out.println("请输入密码：");
-        String passwd = scanner.next();
-//        查询帐号信息
-        String query = "select admin from manager where admin =" + count;
+public  class adminLogin extends JFrame{
+    //标签
+    private JLabel lable1;
+    private JLabel lable2;
+    //文本框
+    private JTextField text1;
+    private JPasswordField text2;
+    //按钮
+    private JButton bt1;
+    private JButton bt2;
+    //构造函数
+    public adminLogin()
+    {
+        this.init();
+        this.addComponent();
 
-        Statement statement = dbCon.getCon().createStatement();
-        ResultSet resultSet;
-//        执行操作语句
-        resultSet = statement.executeQuery(query);
+    }
 
-        if (!resultSet.next()) {
-//            System.out.println(resultSet.getString(1) + resultSet.getString(2));
-            System.out.println("帐号不存在!");
-        }
-        else {
-            query = "select passwd from manager where passwd = " + passwd;
-            resultSet = statement.executeQuery(query);
-            if (!resultSet.next()) {
-                System.out.println("帐号或密码错误！");
+    public void init()
+    {
+        this.setSize(500,400);
+        this.setVisible(true);
+        this.setTitle("管理员登录界面");
+        this.setLayout(null);
+        this.setLocation(700, 300);
+    }
+    private void addComponent()
+    {
+        lable1 = new JLabel("用户名");
+        lable1.setSize(100,70);
+        lable1.setLocation(100,80);
+        this.add(lable1);
+        lable2 = new JLabel("密    码");
+        lable2.setSize(100,70);
+        lable2.setLocation(100,130);
+        this.add(lable2);
+
+        text1 = new JTextField();
+        text1.setSize(150,30);
+        text1.setLocation(160,100);
+        this.add(text1);
+        text2 = new JPasswordField();
+        text2.setSize(150,30);
+        text2.setLocation(160,150);
+        this.add(text2);
+
+        bt1 = new JButton("登录");
+        bt1.setSize(70,30);
+        bt1.setLocation(140,195);
+        this.add(bt1);
+        bt1.addActionListener(e->{
+            DbCon dbCon=new DbCon();
+            String name=text1.getText();
+            String passwd=text2.getText();
+            String query = "select admin from manager where admin ="+"'"+name+"'";
+            System.out.println(query);
+            //    查询帐号信息
+            Statement statement = null;
+            try {
+                statement = dbCon.getCon().createStatement();
+            } catch (Exception exception) {
+                exception.printStackTrace();
             }
-            else {
-                System.out.println("登陆成功！");
+            ResultSet resultSet = null;
+            //        执行操作语句
+            try {
+                resultSet = statement.executeQuery(query);
+            } catch (SQLException throwables) {
+                throwables.printStackTrace();
             }
+            try {
+                if (!resultSet.next()) {
+                    JOptionPane.showMessageDialog(null, "账号错误");
+                }
+                else {
+                    query = "select passwd from manager where passwd = " + "'"+passwd+"'";
 
-        }
+                    try {
+                        resultSet = statement.executeQuery(query);
+                    } catch (SQLException throwables) {
+                        throwables.printStackTrace();
+                    }
+                    System.out.println(resultSet);
+                    if (!resultSet.next()) {
+                        JOptionPane.showMessageDialog(null, "密码错误");
+                    } else {
+//                        new MainWindow();
+                        dispose();
+                    }
+                }
+            } catch (SQLException throwables) {
+                throwables.printStackTrace();
+            }
+        });
+        bt2 = new JButton("退出");
+        bt2.setSize(70,30);
+        bt2.setLocation(250,195);
+        this.add(bt2);
+        bt2.addActionListener(e->{
+            dispose();//关闭当前窗口
+            //System.exit(0);
+        });
+        this.setBackground(Color.blue);
+        //设置单击关闭按钮时的默认操作
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+
+
+    public static void main(String[] args) {
+        new adminLogin();
     }
 }
--- src/Student/StudentLogin.java
+++ src/Student/StudentLogin.java
@@ -2,40 +2,123 @@ package Student;
 
 import db.*;
 
-import java.sql.*;
+import javax.swing.*;
+import java.awt.*;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.sql.Statement;
-import java.util.Scanner;
-//渣渣生是sb
-public class StudentLogin {
-    private static DbCon dbCon = new DbCon();
-    public static void main(String[] args) throws Exception {
-        Scanner scanner=new Scanner(System.in);
-        System.out.println("请输入账号：");
-        String count=scanner.next();
-        System.out.println("请输入密码：");
-        String passwd=scanner.next();
-//        查询帐号信息
-        String query = "select admin from manager where admin =" + count;
 
-        Statement statement = dbCon.getCon().createStatement();
-        ResultSet resultSet;
-//        执行操作语句
-        resultSet = statement.executeQuery(query);
+public class StudentLogin extends JFrame{
+    //标签
+    private JLabel lable1;
+    private JLabel lable2;
+    //文本框
+    private JTextField text1;
+    private JPasswordField text2;
+    //按钮
+    private JButton bt1;
+    private JButton bt2;
+    //构造函数
+    public StudentLogin()
+    {
+        this.init();
+        this.addComponent();
 
-        if (!resultSet.next()) {
-//            System.out.println(resultSet.getString(1) + resultSet.getString(2));
-            System.out.println("帐号不存在!");
-        }
-        else {
-            query = "select passwd from manager where passwd = " + passwd;
-            resultSet = statement.executeQuery(query);
-            if (!resultSet.next()) {
-                System.out.println("帐号或密码错误！");
+    }
+
+    public void init()
+    {
+        this.setSize(500,400);
+        this.setVisible(true);
+        this.setTitle("学生登录界面");
+        this.setLayout(null);
+        this.setLocation(700, 300);
+    }
+    private void addComponent()
+    {
+        lable1 = new JLabel("账    号");
+        lable1.setSize(100,70);
+        lable1.setLocation(100,80);
+        this.add(lable1);
+        lable2 = new JLabel("密    码");
+        lable2.setSize(100,70);
+        lable2.setLocation(100,130);
+        this.add(lable2);
+
+        text1 = new JTextField();
+        text1.setSize(150,30);
+        text1.setLocation(160,100);
+        this.add(text1);
+        text2 = new JPasswordField();
+        text2.setSize(150,30);
+        text2.setLocation(160,150);
+        this.add(text2);
+
+        bt1 = new JButton("登录");
+        bt1.setSize(70,30);
+        bt1.setLocation(140,195);
+        this.add(bt1);
+        bt1.addActionListener(e->{
+            DbCon dbCon=new DbCon();
+            String name=text1.getText();
+            String passwd=text2.getText();
+            String sql = "select sno from studentInfo where sno="+"'"+name+"'";//
+            System.out.println(sql);
+            //    查询帐号信息
+            Statement statement = null;
+            try {
+                statement = dbCon.getCon().createStatement();
+            } catch (Exception exception) {
+                exception.printStackTrace();
             }
-            else {
-                System.out.println("登陆成功！");
+            ResultSet resultSet = null;
+            //        执行操作语句
+            try {
+                resultSet = statement.executeQuery(sql);
+            } catch (SQLException throwables) {
+                throwables.printStackTrace();
             }
+            try {
+                if (!resultSet.next()) {
+                    JOptionPane.showMessageDialog(null, "账号错误");
+                }
+                else {
+                    sql = "select spasswd from studentInfo where spasswd = " + "'"+passwd+"'";
 
-        }
+                    try {
+                        resultSet = statement.executeQuery(sql);
+                    } catch (SQLException throwables) {
+                        throwables.printStackTrace();
+                    }
+                    System.out.println(resultSet);
+                    if (!resultSet.next()) {
+                        JOptionPane.showMessageDialog(null, "密码错误");
+
+                    } else {
+                        JOptionPane.showMessageDialog(null, "欢迎"+name+"登录");
+//                        new MainWindow();
+                        dispose();
+                    }
+                }
+            } catch (SQLException throwables) {
+                throwables.printStackTrace();
+            }
+        });
+        bt2 = new JButton("退出");
+        bt2.setSize(70,30);
+        bt2.setLocation(250,195);
+        this.add(bt2);
+        bt2.addActionListener(e->{
+            dispose();//关闭当前窗口
+            //System.exit(0);
+        });
+        this.setBackground(Color.blue);
+        //设置单击关闭按钮时的默认操作
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+
+
+    public static void main(String[] args) {
+        new StudentLogin();
     }
 }
