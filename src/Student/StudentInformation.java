dd35f619fb00d9f3b948d673119d697f2469be2d
zzs <2878979986@qq.com>
2021-01-04 11:23:30 +0800

    个人信息完善！

--- src/Student/StudentInformation.java
+++ src/Student/StudentInformation.java
@@ -1,15 +1,15 @@
 package Student;
 
 
+import ArrayListData.StudentInfo;
 import dbOperaction.StudentOper;
 
-import java.sql.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import javax.swing.*;
-public  class StudentInfo extends JDialog implements ActionListener{
+public  class StudentInformation extends JDialog{
     JButton Updatebt;
-    JButton cannel;
+
     JLabel snolabel1;
     JLabel  spasswdlabel1;
     JLabel snamelabel1;
@@ -23,8 +23,10 @@ public  class StudentInfo extends JDialog implements ActionListener{
     JLabel idlabel2;
     JLabel enrollmentlabel2;
     static String sno;
+    StudentInfo info = new StudentInfo();
+//    private String info;
 
-    public StudentInfo(String snum) throws Exception {
+    public StudentInformation(String snum) {
         sno = snum;
         init();
     }
@@ -32,18 +34,30 @@ public  class StudentInfo extends JDialog implements ActionListener{
         this.setSize(600,450);
         this.setLocation(100, 20);
         this.setLayout(null);
+//       StudentOper s=new StudentOper();
 
-
+//        try {
+//            StudentInfo info=new StudentInfo();
+//            s.DisStuInfo(sno);
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
+        try {
+            info=new StudentOper().DisStuInfo(sno);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
         snolabel1=new JLabel("学号");
         snolabel1.setSize(100,30);
         snolabel1.setLocation(100, 60);
         this.add(snolabel1);
 
-        snolabel2=new JLabel();
+        snolabel2=new JLabel(sno);
         snolabel2.setSize(120,30);
         snolabel2.setLocation(220, 60);
+        snolabel2.setText( sno);
+        System.out.println(sno);
         this.add(snolabel2);
-
         spasswdlabel1=new JLabel("密码");
         spasswdlabel1.setSize(100,30);
         spasswdlabel1.setLocation(100, 100);
@@ -52,7 +66,9 @@ public  class StudentInfo extends JDialog implements ActionListener{
         spasswdtext=new JTextField();
         spasswdtext.setSize(120,30);
         spasswdtext.setLocation(220, 100);
+        spasswdtext.setText(info.getSpasswd());
         this.add( spasswdtext);
+        System.out.println(info.getSpasswd());
 
         snamelabel1=new JLabel("姓名");
         snamelabel1.setSize(100,30);
@@ -60,9 +76,12 @@ public  class StudentInfo extends JDialog implements ActionListener{
         this.add( snamelabel1);
 
         snamelabel2=new JLabel();
+//        var name= new StudentOper();
         snamelabel2.setSize(120, 30);
         snamelabel2.setLocation(220, 140);
+        snamelabel2.setText(info.getSname());
         this.add( snamelabel2);
+        System.out.println(info.getSname());
 
         classeslabel1=new JLabel("班级");
         classeslabel1.setSize(100,30);
@@ -72,7 +91,9 @@ public  class StudentInfo extends JDialog implements ActionListener{
         classeslabel2=new JLabel();
         classeslabel2.setSize(120, 30);
         classeslabel2.setLocation(220, 180);
+        classeslabel2.setText(info.getClasses());
         this.add(classeslabel2);
+        System.out.println(info.getClasses());
 
         idlabel1=new JLabel("身份证号");
         idlabel1.setSize(100,30);
@@ -82,7 +103,9 @@ public  class StudentInfo extends JDialog implements ActionListener{
         idlabel2=new JLabel();
         idlabel2.setSize(120, 30);
         idlabel2.setLocation(220, 220);
+        idlabel2.setText(info.getId());
         this.add(idlabel2);
+        System.out.println(info.getId());
 
         enrollmentlabel1=new JLabel("注册时间");
         enrollmentlabel1.setSize(100,30);
@@ -92,42 +115,44 @@ public  class StudentInfo extends JDialog implements ActionListener{
         enrollmentlabel2=new JLabel();
         enrollmentlabel2.setSize(120, 30);
         enrollmentlabel2.setLocation(220, 260);
+        enrollmentlabel2.setText(info.getEnrollment());
+
         this.add(enrollmentlabel2);
 
-        Updatebt=new JButton("修改密码");
-        Updatebt.setSize(85,40);
-        Updatebt.setLocation(150, 320);
+        Updatebt=new JButton("确认修改密码");
+        Updatebt.setSize(110,40);
+        Updatebt.setLocation(300, 320);
         this.add( Updatebt);
         Updatebt.addActionListener(e -> {
-            StudentOper studentOper = new StudentOper();
-
-            try {
-               int result= studentOper.changeSpasswd(sno,spasswdtext.getText());
-                if(result==1){
-                    JOptionPane.showMessageDialog(null, "密码修改成功");
+            if(spasswdtext.getText().equals(info.getSpasswd())){
+                JOptionPane.showMessageDialog(null,"密码未改变！");
+            }else{
+                StudentOper studentOper = new StudentOper();
+                try {
+                    int result= studentOper.changeSpasswd(sno,spasswdtext.getText());
+                    if(result==1){
+                        JOptionPane.showMessageDialog(null, "密码修改成功");
+                    }
+                    else{
+                        JOptionPane.showMessageDialog(null, "密码修改失败");
+                    }
+                } catch (Exception exception) {
+                    exception.printStackTrace();
                 }
-                else{
-                    JOptionPane.showMessageDialog(null, "密码修改失败");
-                }
-            } catch (Exception exception) {
-                exception.printStackTrace();
             }
+
         });
+
         this.setVisible(true);
 
-
-        cannel=new JButton("取消");
-        cannel.setSize(85,40);
-        cannel.setLocation(300, 320);
-        this.add(cannel);
-        cannel.addActionListener(this);
+        this.setLocationRelativeTo(null);
         this.setVisible(true);
     }
 
 
 
-    @Override
-    public void actionPerformed(ActionEvent e) {
+//    @Override
+//    public void actionPerformed(ActionEvent e) {
 //        StudentOper studentOper = new StudentOper();
 //        try {
 //            studentOper.changeSpasswd(1810361232,spasswdtext.getText());
@@ -171,10 +196,10 @@ public  class StudentInfo extends JDialog implements ActionListener{
 //        } catch (Exception ex) {
 //            System.out.println("数据库连接或者是数据库操作失败");
 //        }
-    }
+//    }
     public static void main(String[] args) {
         try {
-            StudentInfo s=new StudentInfo("1810361232");
+            new StudentInformation("1810361232");
         } catch (Exception e) {
             e.printStackTrace();
         }
